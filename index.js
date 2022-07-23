9bd61c3b4d4fb7dfc4c2af216f9e7e94e2ebb7c9
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-23 22:07:00 +0800

    added validation for update and add comment route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
 const MongoUtil = require("./MongoUtil")
-const {getPriorOne} = require("./PriorOne")
+const { getPriorOne } = require("./PriorOne")
 
 //set up express app
 const app = express();
@@ -54,12 +54,12 @@ async function main() {
         }
         let sortCriteria = {};
 
-        if(req.query.sortBy){
-            if(req.query.sortBy == "popularity"){
+        if (req.query.sortBy) {
+            if (req.query.sortBy == "popularity") {
                 sortCriteria = {
                     "likes": -1
                 }
-            } else if (req.query.sortBy == "recentlyAdded"){
+            } else if (req.query.sortBy == "recentlyAdded") {
                 sortCriteria = {
                     "dateCreated": -1
                 }
@@ -74,7 +74,7 @@ async function main() {
         }
 
         if (req.query.dateCreated) {
-            
+
             criteria["dateCreated"] = {
                 //how to code date created is within 1 month of current date
                 "$gte": getPriorOne()
@@ -89,7 +89,7 @@ async function main() {
             }
         }
 
-        if (req.query.difficulty){
+        if (req.query.difficulty) {
             criteria["difficulty"] = {
                 "$eq": req.query.difficulty
             }
@@ -107,18 +107,18 @@ async function main() {
             let minTime = ''
             let maxTime = ''
 
-            if (!req.query.minTimeNeeded){
+            if (!req.query.minTimeNeeded) {
                 minTime = '0'
             } else {
                 minTime = req.query.minTimeNeeded
             }
 
-            if (!req.query.maxTimeNeeded){
+            if (!req.query.maxTimeNeeded) {
                 maxTime = '999'
             } else {
                 maxTime = req.query.maxTimeNeeded
             }
-            
+
             // criteria["timeNeeded"] = {
             //     "$gte": parseInt(req.query.minTimeNeeded),
             //     "$lte": parseInt(req.query.maxTimeNeeded)
@@ -206,7 +206,7 @@ async function main() {
             let debrief = "";
             let likes = 0;
 
-            if(req.body.createdBy){
+            if (req.body.createdBy) {
                 createdBy = req.body.createdBy
             } else {
                 createdBy = [{
@@ -215,43 +215,43 @@ async function main() {
                 }]
             }
 
-            if(req.body.name.length < 100){
+            if (req.body.name.length < 100) {
                 name = req.body.name
             }
 
-            if (req.body.description.length<300){
+            if (req.body.description.length < 300) {
                 description = req.body.description
             }
 
-            if (req.body.tags.length){
+            if (req.body.tags.length) {
                 tags = req.body.tags
             }
 
-            if (req.body.groupSize.length){
+            if (req.body.groupSize.length) {
                 groupSize = req.body.groupSize
             }
 
-            if (req.body.timeNeeded > 0 && req.body.timeNeeded < 999){
+            if (req.body.timeNeeded > 0 && req.body.timeNeeded < 999) {
                 timeNeeded = parseInt(req.body.timeNeeded)
-            } 
+            }
 
-            if (req.body.difficulty){
+            if (req.body.difficulty) {
                 difficulty = req.body.difficulty
             }
 
-            if (req.body.materials.length){
+            if (req.body.materials.length) {
                 materials = req.body.materials
             }
 
-            if (req.body.learningObjectives.length){
+            if (req.body.learningObjectives.length) {
                 learningObjectives = req.body.learningObjectives
             }
 
-            if (req.body.instructions){
+            if (req.body.instructions) {
                 instructions = req.body.instructions
             }
 
-            if (req.body.debrief){
+            if (req.body.debrief) {
                 debrief = req.body.debrief
             }
 
@@ -284,32 +284,83 @@ async function main() {
         }
     })
 
-    app.put("/update-tool/:id", async function (req,res){
+    app.put("/update-tool/:id", async function (req, res) {
         try {
-            let name = req.body.name;
-            let description = req.body.description;
-            let groupSize = req.body.groupSize;
-            let timeNeeded = parseInt(req.body.timeNeeded);
-            let difficulty = req.body.difficulty;
-            let materials = req.body.materials;
-            let learningObjectives = req.body.learningObjectives;
-            let instructions = req.body.instructions;
-            let debrief = req.body.debrief;
-            let tags = req.body.tags
+            // let name = req.body.name;
+            // let description = req.body.description;
+            // let groupSize = req.body.groupSize;
+            // let timeNeeded = parseInt(req.body.timeNeeded);
+            // let difficulty = req.body.difficulty;
+            // let materials = req.body.materials;
+            // let learningObjectives = req.body.learningObjectives;
+            // let instructions = req.body.instructions;
+            // let debrief = req.body.debrief;
+            // let tags = req.body.tags
+
+            let name = "";
+            let description = "";
+            let tags = [];
+            let groupSize = [];
+            let timeNeeded = "";
+            let difficulty = "";
+            let materials = [];
+            let learningObjectives = [];
+            let instructions = "";
+            let debrief = "";
+    
+            if (req.body.name.length < 100) {
+                name = req.body.name
+            }
+
+            if (req.body.description.length < 300) {
+                description = req.body.description
+            }
+
+            if (req.body.tags.length) {
+                tags = req.body.tags
+            }
+
+            if (req.body.groupSize.length) {
+                groupSize = req.body.groupSize
+            }
+
+            if (req.body.timeNeeded > 0 && req.body.timeNeeded < 999) {
+                timeNeeded = parseInt(req.body.timeNeeded)
+            }
+
+            if (req.body.difficulty) {
+                difficulty = req.body.difficulty
+            }
+
+            if (req.body.materials.length) {
+                materials = req.body.materials
+            }
+
+            if (req.body.learningObjectives.length) {
+                learningObjectives = req.body.learningObjectives
+            }
+
+            if (req.body.instructions) {
+                instructions = req.body.instructions
+            }
+
+            if (req.body.debrief) {
+                debrief = req.body.debrief
+            }
 
             await MongoUtil.getDB().collection(TOOLS_COLLECTION_NAME).updateOne({
                 "_id": ObjectId(req.params.id)
-            },{
+            }, {
                 "$set": {
                     "name": name,
-                    "description" : description,
-                    "groupSize" : groupSize,
-                    "timeNeeded" : timeNeeded,
-                    "difficulty" : difficulty,
-                    "materials" : materials,
-                    "learningObjectives" : learningObjectives,
-                    "instructions" : instructions,
-                    "debrief" : debrief,
+                    "description": description,
+                    "groupSize": groupSize,
+                    "timeNeeded": timeNeeded,
+                    "difficulty": difficulty,
+                    "materials": materials,
+                    "learningObjectives": learningObjectives,
+                    "instructions": instructions,
+                    "debrief": debrief,
                     "tags": tags
                 }
             })
@@ -319,23 +370,23 @@ async function main() {
                 "message": "tool updated"
             })
         } catch (e) {
-            res.status (500);
+            res.status(500);
             res.json({
-                "message" : "Internal server error. Please contact administrator"
-            }) 
+                "message": "Internal server error. Please contact administrator"
+            })
             console.log(e)
         }
     })
 
-    app.put("/update-likes/:id", async function(req,res){
-        try{
+    app.put("/update-likes/:id", async function (req, res) {
+        try {
             let likes = parseInt(req.body.likes)
 
             await MongoUtil.getDB().collection(TOOLS_COLLECTION_NAME).updateOne({
                 "_id": ObjectId(req.params.id)
-            },{
+            }, {
                 "$set": {
-                    "likes" : likes
+                    "likes": likes
                 }
             })
             res.status(200);
@@ -343,26 +394,42 @@ async function main() {
                 "message": "tool updated"
             })
         } catch (e) {
-            res.status (500);
+            res.status(500);
             res.json({
-                "message" : "Internal server error. Please contact administrator"
-            }) 
+                "message": "Internal server error. Please contact administrator"
+            })
             console.log(e)
         }
     })
 
-    app.put("/add-comment/:id", async function(req,res){
-        try{
-            let comment_id = new ObjectId()
-            let email = req.body.email
-            let userName = req.body.userName
-            let comments = req.body.comments
+    app.put("/add-comment/:id", async function (req, res) {
+        try {
+            let comment_id = new ObjectId();
+            // let email = req.body.email
+            // let userName = req.body.userName
+            // let comments = req.body.comments
+
+            let email = "";
+            let userName = "";
+            let comments = "";
+
+            if (req.body.email){
+                email = req.body.email
+            } 
+
+            if (req.body.userName){
+                userName = req.body.userName
+            }
+
+            if (req.body.comments){
+                comments = req.body.comments
+            }
 
             await MongoUtil.getDB().collection(TOOLS_COLLECTION_NAME).updateOne({
                 "_id": ObjectId(req.params.id)
-            },{
-                $push : {
-                    comments: {comment_id, userName, email, comments}
+            }, {
+                $push: {
+                    comments: { comment_id, userName, email, comments }
                 }
             })
             res.status(200);
@@ -370,16 +437,16 @@ async function main() {
                 "message": "Comment added"
             })
         } catch (e) {
-            res.status (500);
+            res.status(500);
             res.json({
-                "message" : "Internal server error. Please contact administrator"
-            }) 
+                "message": "Internal server error. Please contact administrator"
+            })
             console.log(e)
         }
     })
 
     app.delete("/delete-tool/:id", async function (req, res) {
-        
+
         await MongoUtil.getDB().collection(TOOLS_COLLECTION_NAME).deleteOne({
             "_id": ObjectId(req.params.id)
         })
@@ -389,63 +456,79 @@ async function main() {
         })
     })
 
-    app.get("/get-comment", async function(req,res){
-        let comment_id = req.query.comment_id
-        let email = req.query.email
-        try{
+    app.get("/get-comment", async function (req, res) {
+        // let comment_id = req.query.comment_id
+        // let email = req.query.email
+        let comment_id = req.query.comment_id;
+        let email = req.query.email;
+
+        if (req.query.comment_id){
+            comment_id = req.query.comment_id
+        }
+
+        if (req.query.email){
+            email = req.query.email
+        }
+
+        try {
             let response = await db.collection(TOOLS_COLLECTION_NAME).aggregate([
                 {
                     "$unwind": "$comments"
                 },
                 {
                     "$match": {
-                        "comments.comment_id" : ObjectId(comment_id),
+                        "comments.comment_id": ObjectId(comment_id),
                         "comments.email": email
                     }
                 },
                 {
                     "$project": {
                         "comment_id": "$comments.comment_id",
-                        "email" : "$comments.email",
+                        "email": "$comments.email",
                         "_id": 0
                     }
                 }
             ]).toArray()
             res.status(200);
             res.json(response)
-        }catch(e){
+        } catch (e) {
             res.status(500)
             res.json({
-                "message" : "Internal server error"
+                "message": "Internal server error"
             })
             console.log(e)
             console.log("Internal server error")
         }
     })
 
-    app.put("/delete-comment/", async function(req,res){
-        let comment_id = req.body.comment_id
+    app.put("/delete-comment/", async function (req, res) {
+        // let comment_id = req.body.comment_id;
+        let comment_id = "";
+        
+        if (req.body.comment_id){
+            comment_id = req.body.comment_id
+        }
 
-        try{
+        try {
             await MongoUtil.getDB().collection(TOOLS_COLLECTION_NAME).updateOne({
                 "comments.comment_id": ObjectId(comment_id)
-            },{
-                "$pull" : {
-                    comments: {"comment_id" : ObjectId(comment_id)}
+            }, {
+                "$pull": {
+                    comments: { "comment_id": ObjectId(comment_id) }
                 }
-    
+
             })
             res.status(200);
             res.json({
                 "message": "Comment deleted"
             })
-        } catch (e){
-            res.status (500);
+        } catch (e) {
+            res.status(500);
             res.json({
-                "message" : "Internal server error. Please contact administrator"
+                "message": "Internal server error. Please contact administrator"
             })
             console.log(e)
-        }  
+        }
     })
 
     app.get("/users", async function (req, res) {
